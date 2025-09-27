65cb2ec077f3e541bbe667a03f6bd80939d67c2f
Your Name <stitchsaudio@gmail.com>
2025-09-27 13:40:38 +0200

    m

--- src/components/ServicesPageServices.jsx
+++ src/components/ServicesPageServices.jsx
@@ -18,7 +18,7 @@ if (width < 900) {
             <div className="servicesPageMobileImage"></div>
 
             <h2 className="servicesPageMobileHeading">WEBSITE DEVELOPMENT</h2>
-            <p className="servicesPageMobileParagraph">Losing customers to an outdated website? Our modern, mobile-friendly websites not only look beautiful – they actually get sales. Our layouts have been proven to drive conversion rates, and we offer premium SEO services to help your website rank on Google. <br></br><br></br>Whether you need a simple landing page or a full-blown online store, we’ll make sure your website brings the results your business deserves.</p>
+            <p className="servicesPageMobileParagraph">Losing customers to an outdated website? Our modern, mobile-friendly websites not only look beautiful – they actually get sales. Our layouts have been proven to drive conversion rates, and we offer premium SEO services to help your website rank on Google. <span className="smallerSpacing"><br></br><br></br></span>Whether you need a simple landing page or a full-blown online store, we’ll make sure your website brings the results your business deserves.</p>
             <button className="servicesPageServiceButton">Learn More</button>
         </div>
         
@@ -26,7 +26,7 @@ if (width < 900) {
             <div className="servicesPageMobileImage"></div>
 
             <h2 className="servicesPageMobileHeading">GRAPHIC DESIGN</h2>
-            <p className="servicesPageMobileParagraph">Your brand deserves to stand out. Our graphic design team creates stunning graphics that help you stand out from the crowd, capture potential customers’ attention, and grow your business overall. <br></br><br></br>Whether you need a logo, flyers, or other marketing material, you can trust us to design the most effective visuals.</p>
+            <p className="servicesPageMobileParagraph">Your brand deserves to stand out. Our graphic design team creates stunning graphics that help you stand out from the crowd, capture potential customers’ attention, and grow your business overall. <span className="smallerSpacing"><br></br><br></br></span>Whether you need a logo, flyers, or other marketing material, you can trust us to design the most effective visuals.</p>
             <button className="servicesPageServiceButton">Learn More</button>
         </div>
 
@@ -34,7 +34,7 @@ if (width < 900) {
             <div className="servicesPageMobileImage"></div>
 
             <h2 className="servicesPageMobileHeading">SOCIAL MEDIA MANAGEMENT</h2>
-            <p className="servicesPageMobileParagraph">Posting great content, but getting no results? We create social media strategies that not only help you grow your following, but also turns your existing followers into loyal customers. <br></br><br></br>Which platform should you use? What kind of content should you post? Contact us and have our social media management team answer all your questions and create a strategy that works.</p>
+            <p className="servicesPageMobileParagraph">Posting great content, but getting no results? We create social media strategies that not only help you grow your following, but also turns your existing followers into loyal customers. <span className="smallerSpacing"><br></br><br></br></span>Which platform should you use? What kind of content should you post? Contact us and have our social media management team answer all your questions and create a strategy that works.</p>
             <button className="servicesPageServiceButton">Learn More</button>
         </div>
 
@@ -42,7 +42,7 @@ if (width < 900) {
             <div className="servicesPageMobileImage"></div>
 
             <h2 className="servicesPageMobileHeading">SOCIAL MEDIA<br></br>CONTENT CREATION</h2>
-            <p className="servicesPageMobileParagraph">Don’t have the time to post on social media? We create scroll-stopping social media content with a focus on growing your brand and driving sales. Our excellent social media team stays up to date with current trends, crafting highly engaging graphics and videos that drive sales and puts your brand out there. <br></br><br></br>Focus on running your business – while we focus on growing it.</p>
+            <p className="servicesPageMobileParagraph">Don’t have the time to post on social media? We create scroll-stopping social media content with a focus on growing your brand and driving sales. Our excellent social media team stays up to date with current trends, crafting highly engaging graphics and videos that drive sales and puts your brand out there. <span className="smallerSpacing"><br></br><br></br></span>Focus on running your business – while we focus on growing it.</p>
             <button className="servicesPageServiceButton">Learn More</button>
         </div>
 
