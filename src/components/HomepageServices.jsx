569d2cddd0552162252963cdca4a0e7cfd8ea694
Your Name <stitchsaudio@gmail.com>
2025-09-28 10:36:43 +0200

    m

--- src/components/HomepageServices.jsx
+++ src/components/HomepageServices.jsx
@@ -1,8 +1,5 @@
 import { useRef, useEffect, useState } from 'react'
 import './CSS/HomepageServices.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import useWindowDimensions from '../components/Hooks/useWindowDimensions'
 
 function HomepageServices() {
@@ -14,199 +11,67 @@ function HomepageServices() {
         setServicesState(value);
     }
 
-if (width < 840) {
     return (
       <>
         <div className="HomepageServices">
             <h2 className="homepageOurServices">OUR SERVICES</h2>
 
-            <div className={toggleServicesState === 0 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(3)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">WEB DEVELOPMENT</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceRight">
-
-                        </div>
-
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Losing customers to an outdated website? Our modern, mobile-friendly websites not only look beautiful – they actually get sales. Our layouts have been proven to drive conversion rates, and we offer premium SEO services to help your website rank on Google. <br></br><br></br>Whether you need a simple landing page or a full-blown online store, we’ll make sure your website brings the results your business deserves.</p>
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
+            <div className="homepageServicesColumns">
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">WEBSITE DEVELOPMENT:</h3>
+                    <p className="homepageServiceParagraph">Losing customers to an outdated website? Our modern, mobile-friendly websites not only look beautiful – they actually get sales.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
                 </div>
 
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(1)}/>
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">GRAPHIC DESIGN:</h3>
+                    <p className="homepageServiceParagraph">Your brand deserves to stand out. Our graphic design team creates stunning graphics that help you stand out from the crowd, capture potential customers’ attention, and grow your business overall.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
+                </div>
+
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">SOCIAL MEDIA MANAGEMENT:</h3>
+                    <p className="homepageServiceParagraph">Posting great content, but getting no results? We create social media strategies that not only help you grow your following, but also turns your existing followers into loyal customers.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
+                </div>
             </div>
 
-            <div className={toggleServicesState === 1 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(0)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">GOOGLE/META ADS MANAGEMENT</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceRight">
-
-                        </div>
-
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Our Google/Meta Ad campaigns ensure that you’re seen by the right customers at the right time. <br></br><br></br>Our optimized, targeted ad campaigns drive real results – more interested, qualified leads, more sales, and a higher return on investment.</p>
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
+            <div className="homepageServicesColumns">
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">SOCIAL MEDIA CONTENT CREATION:</h3>
+                    <p className="homepageServiceParagraph">Don’t have the time to post on social media? We create scroll-stopping social media content with a focus on growing your brand and driving sales. Our excellent social media team stays up to date with current trends, crafting highly engaging graphics and videos that drive sales and puts your brand out there.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
                 </div>
 
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(2)}/>
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">GOOGLE ADS MANAGEMENT:</h3>
+                    <p className="homepageServiceParagraph">Our Google Ad campaigns ensure that you’re seen by the right customers at the right time. Our optimized, targeted ad campaigns drive real results – more interested, qualified leads, more sales, and a higher return on investment.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
+                </div>
+
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">META/FACEBOOK ADS MANAGEMENT:</h3>
+                    <p className="homepageServiceParagraph">Stop boosting posts and start running campaigns that actually convert. Our Meta/Facebook ad campaigns focus on generating interested, qualified leads and driving sales. We use precise targeting and eye-catching graphic material – so that you can use Meta/Facebook Ads as a powerful tool for your business’ growth.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
+                </div>
             </div>
 
-            <div className={toggleServicesState === 2 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(1)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">GRAPHIC DESIGN</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceRight">
-
-                        </div>
-
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Your brand deserves to stand out. Our graphic design team creates stunning graphics that help you stand out from the crowd, capture potential customers’ attention, and grow your business overall. <br></br><br></br>Whether you need a logo, flyers, or other marketing material, you can trust us to design the most effective visuals.</p>
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
+            <div className="homepageServicesColumns spaceEvenly">
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">AD CREATION:</h3>
+                    <p className="homepageServiceParagraph">Know how to run targeted, optimized ad campaigns, but don’t have the material to use? Our Ad Creation team specializes in creating aesthetically stunning, high converting ads that help you save money on ad spend, generate higher quality leads, and make more sales.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
                 </div>
 
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(3)}/>
-            </div>
-
-            <div className={toggleServicesState === 3 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(2)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">SOCIAL MEDIA MANAGEMENT/CONTENT CREATION</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceRight">
-
-                        </div>
-
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Don’t have the time to post on social media? We create scroll-stopping social media content with a focus on growing your brand and driving sales. Our excellent social media team stays up to date with current trends, crafting highly engaging graphics and videos that drive sales and puts your brand out there. <br></br><br></br>Focus on running your business – while we focus on growing it.</p>
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
+                <div className="homepageService">
+                    <h3 className="homepageServiceHeading">SEARCH ENGINE OPTIMIZATION:</h3>
+                    <p className="homepageServiceParagraph">Our SEO services help your business rank for valuable keywords, attract qualified traffic, and generate more leads. From keyword research to link building, we use proven strategies to ensure your business is as visible as it can be and ensure long-term results.</p>
+                    <button className="homepageServicesServiceButton">Learn More</button>
                 </div>
-
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(0)}/>
             </div>
         </div>
       </>
     )
-} else {
-    return (
-      <>
-        <div className="HomepageServices">
-            <h2 className="homepageOurServices">OUR SERVICES</h2>
-
-            <div className={toggleServicesState === 0 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(3)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">WEB DEVELOPMENT</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Losing customers to an outdated website? Our modern, mobile-friendly websites not only look beautiful – they actually get sales. Our layouts have been proven to drive conversion rates, and we offer premium SEO services to help your website rank on Google. <br></br><br></br>Whether you need a simple landing page or a full-blown online store, we’ll make sure your website brings the results your business deserves.</p>
-                        </div>
-
-                        <div className="homepageServiceRight">
-
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
-                </div>
-
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(1)}/>
-            </div>
-
-            <div className={toggleServicesState === 1 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(0)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">GOOGLE/META ADS MANAGEMENT</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Our Google/Meta Ad campaigns ensure that you’re seen by the right customers at the right time. <br></br><br></br>Our optimized, targeted ad campaigns drive real results – more interested, qualified leads, more sales, and a higher return on investment.</p>
-                        </div>
-
-                        <div className="homepageServiceRight">
-
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
-                </div>
-
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(2)}/>
-            </div>
-
-            <div className={toggleServicesState === 2 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(1)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">GRAPHIC DESIGN</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Your brand deserves to stand out. Our graphic design team creates stunning graphics that help you stand out from the crowd, capture potential customers’ attention, and grow your business overall. <br></br><br></br>Whether you need a logo, flyers, or other marketing material, you can trust us to design the most effective visuals.</p>
-                        </div>
-
-                        <div className="homepageServiceRight">
-
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
-                </div>
-
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(3)}/>
-            </div>
-
-            <div className={toggleServicesState === 3 ? "homepageService" : "homepageServiceBlock homepageHidden"}>
-                <FontAwesomeIcon className="homepageChevronLeft" icon={faChevronLeft} onClick={() => toggleServices(2)}/>
-
-                <div className="homepageServiceBlock">
-                    <h3 className="homepageServiceBlockHeading">SOCIAL MEDIA MANAGEMENT/CONTENT CREATION</h3>  
-
-                    <div className="homepageServiceBlockColumns">
-                        <div className="homepageServiceLeft">
-                            <p className="homepageServiceCopy">Don’t have the time to post on social media? We create scroll-stopping social media content with a focus on growing your brand and driving sales. Our excellent social media team stays up to date with current trends, crafting highly engaging graphics and videos that drive sales and puts your brand out there. <br></br><br></br>Focus on running your business – while we focus on growing it.</p>
-                        </div>
-
-                        <div className="homepageServiceRight">
-
-                        </div>
-                    </div>
-
-                    <button className="homepageServiceView">View Service</button>
-                </div>
-
-                <FontAwesomeIcon className="homepageChevronRight" icon={faChevronRight} onClick={() => toggleServices(0)}/>
-            </div>
-        </div>
-      </>
-    )
-  }
 }
 
 export default HomepageServices
