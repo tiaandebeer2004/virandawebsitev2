2aff85349f1bed4899ed7ae9e4dde011154175fa
Your Name <stitchsaudio@gmail.com>
2025-09-29 08:48:26 +0200

    m

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -23,7 +23,7 @@ function Banner() {
 
                 <div className="getQuote">
                     <p className="moreClients">Taking on more clients? Stop hoping, start closing.</p>
-                    <button className="contactUs">FREE GROWTH SESSION</button>
+                    <button className="contactUs">Free Growth Session</button>
                 </div>
             </div>
         </div>
